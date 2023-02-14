a876d4276705336498c2848138241890c42e1291
Thiago <thiagodsantos316@gmail.com>
2023-02-13 21:23:17 -0300

    Update indes.js

--- src/indes.js
+++ src/indes.js
@@ -1,1 +1,32 @@
+const { Client } = require("pg");
+const Redis = require("ioredis");
+
+const postgresClient = new Client({
+  host: 'localhost',
+  port: 32768,
+  database: 'dockerredis',
+  user: 'postgres',
+  password: 'postgrespw'
+});
+
+const redisClient = new Redis({
+  host: "localhost",
+  port: 6379
+});
+
+postgresClient.connect().then(() => {
+  return postgresClient.query("SELECT * FROM users");
+}).then((res) => {
+  const data = res.rows;
+
+  // Insere cada linha retornada da consulta SQL no Redis
+  data.forEach((row) => {
+    redisClient.set(row.id, JSON.stringify(row));
+  });
+
+  postgresClient.end();
+  redisClient.quit();
+}).catch((err) => {
+  console.error(err);
+});
 
